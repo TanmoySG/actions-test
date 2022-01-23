1021f2f936938c9e47375b939e5c7f0897396714
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-01-23 19:50:39 +0000

    Test file

--- auto-increment.py
+++ auto-increment.py
@@ -1,6 +1,13 @@
 import json
 
 with open("config.json", "r") as testFile:
-  jsObj = json.load(testFile)
-  
+    jsObj = json.load(testFile)
+
 major, minor, bug = jsObj["version"].split(".")
+
+major = int(major)+1
+
+jsObj["version"] = str(major)+"."+str(minor)+"."+str(bug)
+
+with open("config.json", "w") as testFile:
+	json.dump(jsObj, testFile, indent=4)
